e2454f0cc256d5ba922fe6a3e173997785eac3ca
Anastasiia Dashko <anastasiia.dashko@student.tuke.sk>
2021-11-16 15:06:40 +0100

    app

--- src/App.js
+++ src/App.js
@@ -6,21 +6,23 @@ import Banner from "./Banner";
 import Nav from "./Nav";
 
 function App() {
-  return (
-    <div className="App">
-        <Nav/>
-        <Banner />
-        <Row title="NETFLIX ORIGINALS1" fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-        />
-        <Row title="Trending now" fetchUrl={requests.fetchTrending}/>
-        <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+    return (
+        <div className="App">
+            <Nav/>
+            <Banner/>
+            <Row title="NETFLIX ORIGINALS1" fetchUrl={requests.fetchNetflixOriginals}
+                 isLargeRow
+            />
+            <Row title="Trending now" fetchUrl={requests.fetchTrending}/>
+            <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+            <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
 
-        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
-        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
-    </div>
-  );
+            <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+            <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+        </div>
+    );
+
+
 
 }
 
